b793d1b5a95c96e4c67696c86a1695d3379541e1
Franck Lecerf <franck.lecerf@university-365.com>
2024-01-17 11:58:47 +0100

    premier fichier

---
+++ index.php
@@ -0,0 +1,16 @@
+<H1>en construction</H1>
+
+
+<HR>
+Pour l'instant je n'ai qu'une chanson test
+
+<A href='page_chanson.php'> le fichier CRD entier</A><BR>
+<A href='test_paroles.php'> les paroles seules</A><BR>
+<A href='test_liste_accords.php'> les accords seuls</A><BR>
+<A href='index_accords.php'> la liste des accords</A><BR>
+
+<HR color="red">
+
+
+<A href='liste_interpretes.php'> la liste des interpretes</A><BR>
+<A href='liste_chansons.php'> la liste des chansons</A><BR>
